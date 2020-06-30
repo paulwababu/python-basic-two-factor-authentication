77cc149e75d7d27efdbfe2b4def4b465318bd006
paulwababu <44939805+paulwababu@users.noreply.github.com>
2020-06-30 21:05:16 +0300

    Update 2fa.py

--- 2fa.py
+++ 2fa.py
@@ -1,20 +1,42 @@
+#generate a random 4 digit number
+#initialize and use the SMS api from africa's talking
+#connect to gcloud remote mysql database
+#send 2fa code generated to the database-verify on table fa
+
 import africastalking
 import os
 import secrets
+import mysql.connector
+
+#variable for connecting to remote database
+mydb = mysql.connector.connect(
+    host = "35.232.88.217",
+    user = "test_remote",
+    password = "test_remote",
+    database = "verify"
+)
+
+mycursor = mydb.cursor()
 
 
 #initialize the sdk
-username = "TruthWifiPlatform"
+username = "#"
 api_key = "#"
 africastalking.initialize(username,api_key)
 
 #recipients
-recipients = ['#']
-
+recipients = ['+254796919703']
 
 #generate random 4 digit PIN
 secret_key = secrets.randbelow(10000)
-message = "Your PIN is %04d" %secret_key
+message = ("%04d") %secret_key
+code = message
+
+sql=("INSERT INTO fa (address) VALUES ({})".format(code))
+
+mycursor.execute(sql, code)
+
+mydb.commit()
 
 #initialize the service, in our case, SMS
 sms = africastalking.SMS
